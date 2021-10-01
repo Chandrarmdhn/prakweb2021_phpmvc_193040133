d48085400511e4df0e4da696d77bd8e3c07c3c3c
unknown <e3000yoisantuy@gmail.com>
2021-10-01 14:41:50 +0700

    membuat file view dan asset dimana sudah menggunakan bosstrap untuk mengubah tampilan

---
+++ app/views/about/index.php
@@ -0,0 +1,8 @@
+
+
+
+<div class="container">
+  <h1 class="mt-4">About Me</h1>
+  <img src="<?= BASEURL;?>/img/aw.jpg" alt="chandra" width="200" class="rounded-circle shadow">
+  <p>Hallo, nama saya <?= $data['nama']; ?>, umur saya <?= $data['umur']; ?>, saya adalah seorang <?= $data['pekerjaan']; ?> </p>
+</div>
---
+++ app/views/home/index.php
@@ -0,0 +1,14 @@
+
+<div class="container">
+  <div class= "jumbotron mt-5">
+  <h1 class="display-4">Selamat Datang di Website Saya</h1>
+  <p class="lead">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatum assumenda inventore eius doloribus aut nulla atque in blanditiis excepturi maiores. Rem, sapiente aut? Eveniet, laboriosam odio quam officiis unde reprehenderit.</p>
+  <hr class="my-4">
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit rerum omnis voluptatibus tenetur harum, nihil, id eaque optio, iste rem quam fugiat. Quam, nisi consectetur? Enim sapiente labore dolorum ut?</p>
+  <a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+
+
+
+
+  </div>
+</div>
